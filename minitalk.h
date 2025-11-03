01347d4b66721bc223c5dd8632301408c585e1a3
Eyad Naksho <eyadnaksho@pop-os.fritz.box>
2025-11-03 14:37:12 +0100

    server displays pid

--- minitalk.h
+++ minitalk.h
@@ -2,6 +2,7 @@
 # define MINITALK_H
 
 # include <signal.h>
+# include <sys/types.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include "libft/libft.h"
--- server.c
+++ server.c
@@ -1,6 +1,25 @@
 #include "minitalk.h"
 
+void	handle_signal(int sig)
+{
+	(void)sig;
+}
+
 int	main(void)
 {
+	struct sigaction	sa;
+	pid_t				pid;
+
+	pid = getpid();
+	write(1, "Server PID: ", 12);
+	ft_putnbr_fd(pid, 1);
+	write(1, "\n", 1);
+	sa.sa_handler = handle_signal;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
 	return (0);
 }
