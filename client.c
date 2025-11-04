342cfde4ae5e46e7308c24b210398d202e4795a2
Eyad Naksho <eyad.naqsho@gmail.com>
2025-11-04 17:26:51 +0100

    client argument parsing

--- client.c
+++ client.c
@@ -2,7 +2,19 @@
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	pid_t	server_pid;
+
+	if (argc != 3)
+	{
+		write(2, "Error: Invalid arguments\n", 25);
+		write(2, "Usage: ./client <server_pid> <message>\n", 40);
+		return (1);
+	}
+	server_pid = ft_atoi(argv[1]);
+	if (server_pid <= 0)
+	{
+		write(2, "Error: Invalid PID\n", 19);
+		return (1);
+	}
 	return (0);
 }
